6d6d696249a47efdcdf8d58f3837d5a225a4f2cf
Rajeev Tikka <rajeevtikka@yahoo.com>
2026-09-23 22:47:37 -0700

    react and bun project

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage]  = useState('');
+
+  useEffect(() => {
+    fetch("/api/hello")
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+  })
+
+  return <p>{message}</p>
+}
+
+export default App
