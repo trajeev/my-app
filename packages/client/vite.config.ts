f22a795bb69f239fef08084bc033310f20405d49
Rajeev Tikka <rajeevtikka@yahoo.com>
2026-09-23 23:50:44 -0700

    setup shadcn

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000"
