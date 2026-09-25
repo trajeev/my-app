e3696616be368852ec161ee775ecb736ce0e1b63
Rajeev Tikka <rajeevtikka@yahoo.com>
2026-09-24 23:43:41 -0700

    setup prettier

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,19 +1,19 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000"
-    }
-  }
-})
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   server: {
+      proxy: {
+         '/api': 'http://localhost:3000',
+      },
+   },
+});
